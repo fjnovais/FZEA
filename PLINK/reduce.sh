941fc237022875bc18593b7f6dce2f3e387717c8
Francisco Novais <fjnovais.zoot@gmail.com>
2017-07-24 13:53:03 -0300

    Update reduce.sh

--- PLINK/reduce.sh
+++ PLINK/reduce.sh
@@ -11,6 +11,7 @@ cat $MAP | awk '{print $1}' > snplist
 
 rm snplist 
 
+__________________________________________________________________________________________________________________________________________________________________________
 at $MAP | awk -F"," 'NR>1 {if($2!="0" && $2!="X" && $2!="Y" && $2!="MT") print $1}' | sed -e 's/PAGLDMax_//g' -e 's/ZTSLD2.0_//g' > snplist_${DENSITY}
 
 ./plink --cow --noweb --nonfounders --bfile $BREED --no-parents --no-fid --no-sex --no-pheno --extract snplist_$DENSITY --make-bed --out $DENSITY'_'$BREED
